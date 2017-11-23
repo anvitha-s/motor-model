5afb6603cb822d47bc78dec462eda4d72a2dbbed
anvitha-s <anvi.1208@gmail.com>
2017-11-24 04:32:56 +0530

    documentation added

--- increment.m
+++ increment.m
@@ -1,33 +1,48 @@
+%force exterted on motor due to potential field
 function [index,force] = increment(t,x)
-v1=0;
-L=100;
+%flat potential v1
+v1_t=0;
+%time step
 dt=0.001;
-V_amp=200;%in mV
+%amplitude of sawtooth
+V_amp=200;
+%fraction of sawtooth with positive slope
 a=0.3;
+%length of one sawtooth field in x
+L=100;
+%generating sawtooth potential field v2_t
 v2_u = 1:V_amp/(a*L):V_amp;
 v2_d = V_amp - V_amp/(L*(1-a)):-(V_amp/(L*(1-a))):0;
 v2 = [v2_u,v2_d];
 v2_t = [v2,v2,v2,v2,v2,v2,v2,v2,v2,v2];
 plot(v2_t);
+%time period of cycle of swithching
 T = 1;
+%time for which sawtooth potential is switched on
 t_on = 0.8*T;
 t_off = T - t_on;
+% kT - boltzmann constant*temperature(300K) 
 kT = 26;
 if (t*dt - floor(t*dt)) <= t_on 
+    %sawtooth potential is on
     v = V_amp/kT;
     if mod(x,100) < a*L
+        %upward slope of sawtooth potential
         disp('mod');
         disp(mod(x,100));
         disp('x');
         disp(x);
         force = -v/a;
     else
+        %downward slope of sawtooth potential
         force = v/(1-a);
     end
         disp('force');
     disp(force);
+    %index denotes v2 is on
     index=1;
 else
     force = 0;
+    %index denotes v1 is on
     index = 0;
 end
--- two_motors_contstant_exchange.m
+++ two_motors_contstant_exchange.m
@@ -1,35 +1,47 @@
-V_amp = 200;
-a=0.3;
-L=100;
-v2_u = 1:V_amp/(a*L):V_amp;
-v2_d = V_amp - V_amp/(L*(1-a)):-(V_amp/(L*(1-a))):0;
-v2 = [v2_u,v2_d];
-v2_t = [v2,v2,v2,v2,v2,v2,v2,v2,v2,v2];
-x(1:10000) = 0;
+%V_amp = 200;
+%length of one sawtooth
+%L=100;
+%coefficient in repulsion equation
+cI=1e4;
+%time step
+dt=0.001;
+%positions of molecular motors at n*dt
+x1(1:10000) = 0;
 x2(1:10000) = 0;
 for i=1:10000
     if i==1
+        %initial time step,  x(0) = 450
         [index,incr] = increment(i,450);
-        x(i) = 450 + (incr*dt + sqrt(2*dt)*randn(1,1));
+        x1(i) = 450 + (incr*dt + sqrt(2*dt)*randn(1,1));
         [index,incr] = increment(i,490);
         x2(i) = 490 + (incr*dt + sqrt(2*dt)*randn(1,1));
     else
-        [index,incr] = increment(i,x(i-1));
-        x(i) = x(i-1) + (incr*dt + sqrt(2*dt)*randn(1,1));
+        %index - index of potential field - v1(flat) or v2
+        %incr - component of time derivative of x due to potential field
+        [index,incr] = increment(i,x1(i-1));
+        %x for next time step 
+        %sqrt(2*dt)*randn(1,1) - change in x due to diffusion
+        x1(i) = x1(i-1) + (incr*dt + sqrt(2*dt)*randn(1,1));
+        %similar computation for 2nd motor with x coordinate x2
         [index,incr] = increment(i,x2(i-1));
         x2(i) = x2(i-1) + (incr*dt + sqrt(2*dt)*randn(1,1));
-        r = x2(i-1) - x(i-1);
+        % r - distance between the two motors in the last time step
+        r = x2(i-1) - x1(i-1);
+        % diameter of both motors taken as 1 unit 
         if abs(r) < 1
-            rep_force = 1e8/r^6;
+            %motors are in contact at x(i-1)
+            %rep_force - repulsion force/unit mass
+            rep_force = cI/r^6;
+            % adding effect of repulsion
             x2(i) = x2(i) + 0.5*sign(r)*rep_force*dt^2;
-            x(i) = x(i) - 0.5*sign(r)*rep_force*dt^2;
+            x1(i) = x1(i) - 0.5*sign(r)*rep_force*dt^2;
         end
     end
-        if(x(i) > 1000 | x(i) < 0)
+    %if motor moves out of frame
+        if(x1(i) > 1000 | x1(i) < 0 | x2(i) > 1000 | x2(i) < 0)
         disp('exiting loop');
         break;
         end
 end
-disp(x);
-plot(x,0.001:0.001:10,x2,0.001:0.001:10);
-plot(1:1:1000,v2_t);
+disp(x1);
+plot(x1,0.001:0.001:10,x2,0.001:0.001:10);
