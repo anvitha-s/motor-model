ba933dd9964907db5ac23efc861240a3c8625c53
anvitha-s <anvi.1208@gmail.com>
2018-01-04 16:49:52 +0530

    stall force added

--- py_code/main.py
+++ py_code/main.py
@@ -4,28 +4,44 @@ import matplotlib.pyplot as plt
 import potential
 import motor
 
+def omegagenerator(**kwargs):
+    if kwargs['type'] == 'uniform':
+        w = [kwargs['value']]*(kwargs['length'] + 1)
+    elif kwargs['type'] == 'dirac':
+        w = []
+        for i in range(int(kwargs['length']/kwargs['peak'])):
+            w = np.append(w,[kwargs['value']])
+            w = np.append(w,[0]*(kwargs['peak'] - 1))
+            print i
+        if kwargs['p_d'] == 0 and kwargs['length']%kwargs['peak'] == 0:
+            w = np.append(w,kwargs['value'])
+        else: 
+            w = np.roll(w,kwargs['p_d'])            
+    return w
+
 V_amp = 600
 a = 0.3
 L = 100
 p_d = 40
-w12 = 0.2 
-w21 = 0.4
-F = 10
+F = 8
+
 p1 = potential.Potential(V_amp,a,L,0)
 p2 = potential.Potential(V_amp,a,L,p_d)
-m1 = motor.Motor(500,p1,p2,1,0.3,1)
-#m2 = motor.Motor(450,p1,p2,1,0.3)
+w12 = omegagenerator(type='dirac',value=1,p_d=0,length=1000,peak=L)
+w21 = omegagenerator(type='dirac',value=1,p_d=p_d,length=1000,peak=L)
+m1 = motor.Motor(500,p1,p2,1,0.3,1,w12,w21)
+m2 = motor.Motor(490,p1,p2,1,0.3,1,w12,w21)
 
 for i in range(999):
-     m1.increment_x(F,1)
-#    m2.increment_x()
-#    motor.checkcollision(m1,m2)
-
-
-print 'velocity = ' + repr(m1.velocity())
-#m1.plotpos_cycle()
+     m1.increment_x(F)
+     m2.increment_x(F)
+     motor.checkcollision(m1,m2)
+print 'motor 1 : velocity = ' + repr(m1.velocity())
+print 'motor 2 : velocity = ' + repr(m2.velocity())
+m1.plotpos_cycle()
 m1.plotpos_state()
-#m2.plotpos_cycle()
+m2.plotpos_state()
+m2.plotpos_cycle()
 p1.plotpot('r')
 p2.plotpot('b')
 plt.show()
--- py_code/motor.py
+++ py_code/motor.py
@@ -6,62 +6,40 @@ import potential
 dt=0.01
 
 class Motor:
-    def __init__(self,x_init,potential1,potential2,t_p,t_jump,init_state):
+    def __init__(self,x_init,potential1,potential2,t_p,t_jump,init_state,w12,w21):
         self.state = 1
         self.x = []
         self.x.append(float(x_init))
         self.pot = [potential1,potential2]
         self.time_period = t_p
         self.transition_time = t_jump
-        self.state = [1]
-
-    def increment_x(self):
-        x_p = self.step_incr(self.x[-1])
-        x_pp = self.step_incr(x_p)
-        self.x.append(float((x_p + x_pp)/2))
+        self.state = [init_state]
+        self.w12 = w12
+        self.w21 = w21
     
-    def step_incr(self,x):
-       s=np.random.normal(0,1)
-       x_p = x + ((2*dt)**(0.5))*s
-       if ((len(self.x)*dt)%self.time_period)  < self.transition_time:
-           self.state.append(1)
-           x_p = x_p +  self.pot[0].incr(x)*dt
-       else:
-           self.state.append(2)
-           x_p = x_p +  self.pot[1].incr(x)*dt
-       return x_p
-
-    def increment_x(self,F_ext,w12,w21=None):
-        if w21 != None: 
-            s = np.random.uniform(0,1)
-            if self.state[-1] == 1:
-                if s < w12:
-                    self.state.append(2)
-                else:
-                    self.state.append(1)
-            else: 
-                if s < w21:
-                    self.state.append(1)
-                else:
-                    self.state.append(2)
-            x_p = self.step_incr(self.x[-1],F_ext,self.state)
-            x_pp = self.step_incr(x_p,F_ext,self.state)
-            self.x.append(float((x_p + x_pp)/2))
-    
-        else:
-            if ((np.floor(self.x[-1] - self.pot[0].delta))%self.pot[0].L) == 0:
-                self.increment_x(F_ext,w12,0)
-            elif ((np.floor(self.x[-1] - self.pot[1].delta))%self.pot[1].L) == 0:
-                self.increment_x(F_ext,0,w12)
-            else:
-                self.increment_x(F_ext,0,0)
-    
-    def step_incr(self,x,F_ext,state):              
+    def step_incr(self,x,F_ext):              
         s=np.random.normal(0,1)
         x_p = x + ((2*dt)**(0.5))*s
         x_p = x_p +  self.pot[self.state[-1]-1].incr(x)*dt - F_ext*dt
         return x_p
-    
+
+    def increment_x(self,F_ext):
+        force_ext = F_ext
+        s = np.random.uniform(0,1)
+        if self.state[-1] == 1:
+            if s < self.w12[int(np.floor(self.x[-1]))]:
+                self.state.append(2)
+            else:
+                self.state.append(1)
+        else: 
+            if s < self.w21[int(np.floor(self.x[-1]))]:
+                self.state.append(1)
+            else:
+                self.state.append(2)
+        x_p = self.step_incr(self.x[-1],force_ext)
+        x_pp = self.step_incr(x_p,force_ext)
+        self.x.append(float((x_p + x_pp)/2))
+
     def velocity(self):
        self.avg_velocity = float((self.x[0] - self.x[-1])/(dt*len(self.x)))
        return self.avg_velocity
@@ -71,18 +49,18 @@ class Motor:
         plt.plot(self.x,x_plot)
 
     def plotpos_state(self):
-        x_1 = []
-        x_2 = []
-        t_1 = []
-        t_2 = []
+        self.x_1 = []
+        self.x_2 = []
+        self.t_1 = []
+        self.t_2 = []
         for i in range(len(self.x)-1):
             if self.state[i] == 1:
-                x_1.append(self.x[i])
-                t_1.append(i*dt)
+                self.x_1.append(self.x[i])
+                self.t_1.append(i*dt)
             if self.state[i] == 2:
-                x_2.append(self.x[i])
-                t_2.append(i*dt)
-        plt.plot(x_1,t_1,'r.',x_2,t_2,'b.')
+                self.x_2.append(self.x[i])
+                self.t_2.append(i*dt)
+        plt.plot(self.x_1,self.t_1,'r.',self.x_2,self.t_2,'b.')
 
     def plotpos_cycle(self):
         x_1 = []
