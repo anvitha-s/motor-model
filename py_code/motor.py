57fca4f8215fb13c6ddbe1b46316c45109c8ad72
anvitha-s <anvi.1208@gmail.com>
2018-03-13 01:05:32 +0530

    motor model non-dimensionalised

--- py_code/motor.py
+++ py_code/motor.py
@@ -3,45 +3,48 @@ import matplotlib.pyplot as plt
 
 import potential
 
-dt=0.01
-
 class Motor:
-    def __init__(self,x_init,potential1,potential2,t_p,t_jump,init_state,w12,w21):
-        self.state = 1
+    def __init__(self,x_init,potential1,potential2,init_state,w12,w21):
         self.x = []
         self.x.append(float(x_init))
         self.pot = [potential1,potential2]
-        self.time_period = t_p
-        self.transition_time = t_jump
         self.state = [init_state]
-        self.w12 = w12
-        self.w21 = w21
+        self.w = [w12,w21]
     
-    def step_incr(self,x,F_ext):              
+    def step_incr(self,x,F_ext,dt,u_0):              
         s=np.random.normal(0,1)
-        x_p = x + ((2*dt)**(0.5))*s
-        x_p = x_p +  self.pot[self.state[-1]-1].incr(x)*dt - F_ext*dt
+        x_p = x + ((2*dt)**(0.5))*s*(self.state[-1]-1)
+        x_p = x_p + u_0*self.pot[self.state[-1]-1].incr(x)*dt + F_ext*dt
         return x_p
 
-    def increment_x(self,F_ext):
+    def increment_x(self,F_ext,dt,u_0):
         force_ext = F_ext
+        self.dt = dt
         s = np.random.uniform(0,1)
-        if self.state[-1] == 1:
-            if s < self.w12[int(np.floor(self.x[-1]))]:
-                self.state.append(2)
-            else:
-                self.state.append(1)
-        else: 
-            if s < self.w21[int(np.floor(self.x[-1]))]:
-                self.state.append(1)
-            else:
-                self.state.append(2)
-        x_p = self.step_incr(self.x[-1],force_ext)
-        x_pp = self.step_incr(x_p,force_ext)
+        #print 'size : '
+        #print int(np.floor(self.x[-1])*100)
+        if self.w[self.state[-1]-1].shift(self.x[-1]):
+            self.state.append(self.state[-1]%2 + 1)
+        else:
+            self.state.append(self.state[-1])
+        #if self.state[-1] == 1:
+        #    if int(np.floor(self.x[-1])*100) > 1001:
+        #        return 'out' 
+        #    if s < self.w12[int(np.floor(self.x[-1])*100)]:
+        #        self.state.append(2)
+        #    else:
+        #        self.state.append(1)
+        #else: 
+        #    if s < self.w21[int(np.floor(self.x[-1]))]:
+        #        self.state.append(1)
+        #    else:
+        #        self.state.append(2)
+        x_p = self.step_incr(self.x[-1],force_ext,dt,u_0)
+        x_pp = self.step_incr(x_p,force_ext,dt,u_0)
         self.x.append(float((x_p + x_pp)/2))
 
     def velocity(self):
-       self.avg_velocity = float((self.x[0] - self.x[-1])/(dt*len(self.x)))
+       self.avg_velocity = float((self.x[-1] - self.x[0])/(self.dt*len(self.x)))
        return self.avg_velocity
     
     def plotposition(self):
@@ -49,32 +52,14 @@ class Motor:
         plt.plot(self.x,x_plot)
 
     def plotpos_state(self):
-        self.x_1 = []
-        self.x_2 = []
-        self.t_1 = []
-        self.t_2 = []
-        for i in range(len(self.x)-1):
-            if self.state[i] == 1:
-                self.x_1.append(self.x[i])
-                self.t_1.append(i*dt)
-            if self.state[i] == 2:
-                self.x_2.append(self.x[i])
-                self.t_2.append(i*dt)
-        plt.plot(self.x_1,self.t_1,'r.',self.x_2,self.t_2,'b.')
-
-    def plotpos_cycle(self):
-        x_1 = []
-        x_2 = []
-        t_1 = []
-        t_2 = []
-        for i in range(1000):
-            if self.state[i] == 1:
-                x_1.append(self.x[i])
-                t_1.append(dt*i)
-            else:
-                x_2.append(self.x[i])
-                t_2.append(dt*i)
-        plt.plot(x_1,t_1,'r.',x_2,t_2,'b.')
+        #self.x_1 = []
+        #self.x_2 = []
+        #self.t_1 = []
+        #self.t_2 = []
+        color = ['r','b']
+        for i in range(len(self.x) - 2):
+            plt.plot([self.x[i],self.x[i+1]],[i*self.dt,(i+1)*self.dt],color[self.state[i]-1])
+            plt.plot(self.x[i],i*self.dt,color[self.state[i]-1] + '.')
 
 r = 1
 c_I = 0.000001
--- py_code/potential.py
+++ py_code/potential.py
@@ -1,6 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Omega:
+    def __init__(self,**kwargs):
+        self.wavelength = 0
+        if kwargs['type'] == 'uniform':
+            self.value = kwargs['value']
+        elif kwargs['type'] == 'dirac':
+            self.wavelength = kwargs['length']
+            self.value = kwargs['value']
+    def shiftuniform(self):
+            s = np.random.uniform(0,1)
+            return (s > self.value)
+    def shift(self, x):
+        if self.wavelength == 0:
+            return self.shiftuniform()
+        else:
+            if int((x*100))%(self.wavelength*100) == 0:
+                return self.shiftuniform()
+            else:
+                return False
+
 class Potential:
     def __init__(self, amplitude, a, wavelength, offset):
         self.amp = amplitude
@@ -18,10 +38,15 @@ class Potential:
     def plotpot(self,color):
         self.field  = []
         self.field.append(0)
-        for i in range(10):
-            self.field.extend(np.linspace(self.amp/self.a,self.amp,num=self.a))
-            self.field.extend(np.linspace(self.amp - (self.amp/(1-self.a)),0,num=(self.L-self.a)))
-        self.field = np.roll(self.field, self.delta)
-        self.field1 = self.field/40
-        x_plot = np.linspace(0,1000,num=1001)
+        for i in range(5):
+            self.field.extend(np.linspace(self.amp/(self.a*100),self.amp,num=self.a*100))
+            #print np.linspace(self.amp/(self.a*100),self.amp,num=self.a*100)
+            self.field.extend(np.linspace(self.amp - (self.amp/((1-self.a)*100)),0,num=(self.L-self.a)*100))
+        #print 'delta ' + repr(self.delta)
+        self.field = np.roll(self.field, int(self.delta*100))
+        self.field1 = self.field
+        x_plot = np.linspace(2,7,num=501)
+       # print self.field
+        #print 'xplot ' + repr(len(x_plot))
+        #print 'self.field ' + repr(len(self.field))
         plt.plot(x_plot,self.field1,color)
